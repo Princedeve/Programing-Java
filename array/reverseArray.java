333430bafb8bddb6eed31885127de286606d3b4f
Prince <prince935059@gmail.com>
2026-03-17 12:07:54 +0530

    update code

---
+++ array/reverseArray.java
@@ -0,0 +1,27 @@
+package array;
+
+public class reverseArray {
+      public static void reverseArray(int arr[]){
+      int first = 0, last = arr.length-1;
+
+      while (first < last) {
+        //swap
+        int temp = arr[last];
+        arr[last] = arr[first];
+        arr[first] = temp;
+        first++;
+        last--;
+      }
+    }
+    public static void main(String args[]){
+        
+        int numbers[] = {2,4,6,8,10};
+        reverseArray(numbers);
+        //print
+        for(int i = 0; i<numbers.length; i++){
+            System.out.print(numbers[i]+" ");
+        }
+        System.out.println();
+        
+    }
+}
