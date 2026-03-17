5d40363ab519ed4f1e9c5d3a22cdeadb3ead36ac
Prince <prince935059@gmail.com>
2026-03-17 19:18:45 +0530

    update code

--- array/pairsArray.java
+++ array/pairsArray.java
@@ -2,12 +2,16 @@ package array;
 
 public class pairsArray {
     public static void pairsMaker(int arr[]){
+    int tp = 0;
     for(int i = 0; i<arr.length; i++){
+    
     for(int j = i+1; j<arr.length; j++){
       System.out.print("("+arr[i]+", "+arr[j]+") ");
+      tp++;
     }
     System.out.println();
     }
+    System.out.println("Total pairs = "+ tp);
   }
     public static void main(String args[]){
         int arr[] = {2,4,6,8,10};
