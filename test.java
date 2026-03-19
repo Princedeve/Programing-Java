cf9ec8e033206f61667b1cc43037d697732c1c6d
Prince <prince935059@gmail.com>
2026-03-19 18:09:25 +0530

    update code

--- test.java
+++ test.java
@@ -1,26 +1,31 @@
 import java.util.*;
 
 public class test {
+  public static int prefixArray(int arr[]){
+    int n = arr.length;
 
-   public static void subArray(int arr[]){
-    int tp = 0;
-    for(int i = 0; i<arr.length; i++){
-      for(int j = i; j<arr.length; j++){
-        int sum = 0;
-        for(int k = i; k<=j; k++){
-          sum += arr[k];
-        }
-        System.out.println("sum of subArray: "+sum+" ");
-        tp++;
+    int prefix[] = new int[n];
+    prefix[0] = arr[0];
+    int max = prefix[0];
+    for(int i = 1; i<n; i++){
+      prefix[i] = prefix[i-1] + arr[i];
+      if(max < prefix[i]){
+        max = prefix[i];
       }
-      System.out.println();
     }
-    System.out.println("Total number or subArray is: "+ tp );
+    return max;
   }
-    public static void main(String args[]){
-      int arr[] = {2,4,6,8,10};
-      subArray(arr);
-        
+  public static void printArray(int arr[]){
+    for(int i = 0; i<arr.length; i++){
+      System.out.print(arr[i]+" ");
     }
-
+    System.out.println();
+  }
+   public static void main(String args[]){
+    int arr[] = {1,-2,6,-1,3};
+    int prefix = prefixArray(arr);
+    System.out.println(prefix);
+    // printArray(prefix);
+   }
+    
 }
