f8e001676d878c08ae76e6559dd47f9077b86d95
Prince <prince935059@gmail.com>
2026-03-19 12:50:21 +0530

    update code

--- array/maxSubArraySum.java
+++ array/maxSubArraySum.java
@@ -1,6 +1,6 @@
 package array;
 // Brute force approch is a worst case O(n3)
-public record maxSubArraySum() {
+public class maxSubArraySum {
     public static void maxSubArraySum(int arr[]){
     int max = 0;
     for(int i = 0; i<arr.length; i++){
