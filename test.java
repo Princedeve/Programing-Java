7ffb6bcca42af2c6ba88d7198de4ab7211188071
Prince <prince935059@gmail.com>
2026-03-20 17:56:23 +0530

    update code

--- test.java
+++ test.java
@@ -1,31 +1,23 @@
 import java.util.*;
 
 public class test {
-  public static int prefixArray(int arr[]){
-    int n = arr.length;
-
-    int prefix[] = new int[n];
-    prefix[0] = arr[0];
-    int max = prefix[0];
-    for(int i = 1; i<n; i++){
-      prefix[i] = prefix[i-1] + arr[i];
-      if(max < prefix[i]){
-        max = prefix[i];
+  public static void kadanesMaxSubArraySum(int arr[]){
+    // using kadane's algorithm
+    int max = 0;
+    int sum = 0;
+    for(int i = 0; i<arr.length; i++){
+      sum += arr[i];
+      if(sum < 0){
+        sum = 0;
+      }
+      if(max < sum){
+        max = sum;
       }
     }
-    return max;
-  }
-  public static void printArray(int arr[]){
-    for(int i = 0; i<arr.length; i++){
-      System.out.print(arr[i]+" ");
-    }
-    System.out.println();
+    System.out.println("Maximum subarray sum is: "+max);
   }
    public static void main(String args[]){
-    int arr[] = {1,-2,6,-1,3};
-    int prefix = prefixArray(arr);
-    System.out.println(prefix);
-    // printArray(prefix);
-   }
-    
+    int arr[] = {-2,-3,4,-1,-2,1,5,-3};
+    maxSubArraySum(arr);
+   }    
 }
