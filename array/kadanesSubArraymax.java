6b9c992e9e57ebbf20dc80dc977c3a614a1ccbd2
Prince <prince935059@gmail.com>
2026-03-20 18:17:23 +0530

    update code

--- array/kadanesSubArraymax.java
+++ array/kadanesSubArraymax.java
@@ -1,23 +1,28 @@
 package array;
 
 public class kadane'sSubArraymax {
-    public static void kadanesMaxSubArraySum(int arr[]){
+   public static void kadanesMaxSubArraySum(int arr[]){
     // using kadane's algorithm
-    int max = 0;
-    int sum = 0;
+    int max = arr[0];
+    int sum = arr[0];
     for(int i = 0; i<arr.length; i++){
       sum += arr[i];
+
+      if(sum > max){
+        max = sum;
+      }
+
       if(sum < 0){
         sum = 0;
       }
-      if(max < sum){
-        max = sum;
-      }
+      
     }
     System.out.println("Maximum subarray sum is: "+max);
   }
    public static void main(String args[]){
     int arr[] = {-2,-3,4,-1,-2,1,5,-3};
-    maxSubArraySum(arr);
-   }     
+    int num[] = {-1,-2,-3,-4};
+    kadanesMaxSubArraySum(arr);
+    kadanesMaxSubArraySum(num);
+   }    
 }
