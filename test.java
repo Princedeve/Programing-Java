70b39321d413686e4e78571ab8595618d0d8e416
Prince <prince935059@gmail.com>
2026-03-21 18:46:20 +0530

    update code

--- test.java
+++ test.java
@@ -3,28 +3,9 @@ import java.util.*;
 import array.maxSubArraySum;
 
 public class test {
-  public static void kadanesMaxSubArraySum(int arr[]){
-    // using kadane's algorithm
-    int max = arr[0];
-    int sum = arr[0];
-    for(int i = 0; i<arr.length; i++){
-      sum += arr[i];
   
-      if(sum > max){
-        max = sum;
-      }
-      
-      if(sum < 0){
-        sum = 0;
-      }
-      
-    }
-    System.out.println("Maximum subarray sum is: "+max);
-  }
    public static void main(String args[]){
-    int arr[] = {-2,-3,4,-1,-2,1,5,-3};
-    int num[] = {-1,-2,-3,-4};
-    kadanesMaxSubArraySum(arr);
-    kadanesMaxSubArraySum(num);
+    int height[] = {4,2,0,6,3,2,5};
+    
    }    
 }
