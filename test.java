83d1d24f8584cf63f9258b9413d96a1368138485
Prince <prince935059@gmail.com>
2026-03-21 18:59:55 +0530

    update code

--- test.java
+++ test.java
@@ -1,11 +1,29 @@
 import java.util.*;
 
-import array.maxSubArraySum;
-
 public class test {
-  
+  public static int[] getLeftMax(int arr[]){
+    int left[] = new int[arr.length];
+    left[0] = arr[0];
+    for(int i = 1; i<arr.length; i++){
+      if(left[i-1] > arr[i]){
+        left[i] = left[i-1];
+      }else{
+        left[i] = arr[i];
+      }
+    }
+    return left;
+  }
+  //   public static int[] getRightMax(int arr[]){
+  //   
+  // }
+  public static void trappingRainWater(int arr[]){
+    
+  } 
    public static void main(String args[]){
     int height[] = {4,2,0,6,3,2,5};
-    
+    int left[] = getLeftMax(height);
+    for(int i = 0; i<left.length; i++){
+      System.out.println(left[i]);
+    }
    }    
 }
