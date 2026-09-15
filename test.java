663a74c1401bba51d147117a75567f71014e1db3
Prince <prince935059@gmail.com>
2026-09-15 13:20:40 +0530

    update code

--- test.java
+++ test.java
@@ -1,38 +1,30 @@
 import java.util.*;
 
+import array.linearSearch; 
+
 public class test {
-  
-  public static void sort(int arr[]){
-    for(int i = 0; i < arr.length; i++){
-      for(int j = i + 1; j < arr.length; j++){
-        if(arr[i] > arr[j]){
-          int temp = arr[i];
-          arr[i] = arr[j];
-          arr[j] = temp;
+    public static int linearSearch(int num[], int key) {
+
+          for(int i = 0; i<num.length; i++){
+            if(num[i] == key){
+               return i;
+            }
         }
-      }
-    }
-  }
 
-  public static boolean checkTwiceOptimise(int arr[]){
-    // sort(arr);
-    Arrays.sort(arr);
-
-    for(int i = 0; i < arr.length - 1; i++){
-      if(arr[i] == arr[i+1]){
-        return true;
-      }
+        return -1;
     }
-    return false;
-  }
- 
-  public static void main(String args[]){
-    int nums[] = {1,2,3,3};
 
-    if(checkTwiceOptimise(nums)){
-      System.out.println("true");
-    } else {
-      System.out.println("false");
+    public static void main(String[] args) {
+        
+        int arr[] = {2,4,6,8,10,12,14,16};
+        int x = 10;
+
+        int index = linearSearch(arr, x);
+
+        if(index == -1){
+            System.out.println("Not found");
+        }else{
+            System.out.println("key is at index: "+index);
+        }
     }
-  }    
 }
