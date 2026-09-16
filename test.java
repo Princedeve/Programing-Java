f7e101bfb237dcd890b9d31b1b737d1fd1173fe5
Prince <prince935059@gmail.com>
2026-09-16 12:58:58 +0530

    update code

--- test.java
+++ test.java
@@ -32,18 +32,44 @@ public class test {
         int mid;
         int st = 0;
         int end = arr.length-1;
-        int result = 0;
         for(int i = st; i<=end; i++){
             mid = (st+end) / 2;
             if(arr[mid] == key ){
-                result = mid;
+                return mid;
             }else if(arr[mid] < key){
                 st = mid+1;
             }else{
                 end = mid-1;
             }
         }
-        return result;
+        return -1;
+    }
+
+    public static int binarySearchWhile(int arr[], int key){
+        int mid;
+        int st = 0;
+        int end = arr.length-1;
+        while(st <= end){
+            mid = (st+end) / 2;
+            if(arr[mid] == key ){
+                return mid;
+            }else if(arr[mid] < key){
+                st = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+        return -1;
+    }
+
+    public static int[] reverseArr(int arr[]){
+      int n = arr.length-1;
+      int copy[] = new int[arr.length];
+
+      for(int i = n; i>=0; i--){
+        copy[n-i] = arr[i];
+      }
+      return copy;
     }
     public static void main(String[] args) {
         
@@ -53,7 +79,22 @@ public class test {
        int array[] = {1,2,6,3,5};
 
        int num[] = {2,4,6,8,10,12,14};
-       int key = 8;
-       System.out.println(binarySearch(num, key));
+       int key = 4;
+       
+       int rev[] = {2,4,6,8,10};
+
+       int copy[] = new int[rev.length-1];
+
+       copy = reverseArr(rev);
+
+       for(int i = 0; i<rev.length; i++){
+        rev[i] = copy[i];
+       }
+
+       for(int i = 0; i<rev.length; i++){
+        System.out.println(rev[i]);
+       }
+       
+
     }
 }
